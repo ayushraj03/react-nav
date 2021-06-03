108cda59fdff37bbe27bfd436cd47cc9654385ca
ayush raj <ayushthebest001@gmail.com>
2021-06-04 02:00:10 +0530

    sync added

--- src/App.js
+++ src/App.js
@@ -2,14 +2,19 @@ import logo from './logo.svg';
 import './App.css';
 import SideBar from "./sidebar";
 import "./styles.css";
+import axios from "axios";
+import React, { useState, useEffect } from 'react';
 
 function App() {
+
   return (
     <div id="App">
       <SideBar pageWrapId={"page-wrap"} outerContainerId={"App"} />
 
       <div id="page-wrap">
-        <h1>Click to show menu</h1>
+        
+        
+        <p>No data</p>
       </div>
     </div>
   );
--- src/sidebar.js
+++ src/sidebar.js
@@ -1,9 +1,37 @@
-import React from "react";
 import { slide as Menu } from "react-burger-menu";
+import axios from "axios";
+import React, { useState, useEffect } from 'react';
+import styled from "styled-components";
 
 export default props => {
+
+  const [status, setStatus] = useState("No data");
+
+  const Button = styled.button`
+    background-color: black;
+    color: white;
+    font-size: 20px;
+    padding: 10px 60px;
+    border-radius: 5px;
+    margin: 10px 0px;
+    cursor: pointer;
+`;
+
+function handleClick () {
+  axios.get(`https://jsonplaceholder.typicode.com/users`)
+  .then(res => {
+    var data = res.data
+    console.log("Success")
+    setStatus("Data loaded Successfully!")
+    
+  }).catch(e => {
+    console.log('Setting the status after ERROR!')
+    setStatus("Error fetching the API")
+  });
+}
   return (
     <Menu {...props}>
+      <Button onClick={handleClick}>Sync</Button>
       <a className="menu-item" href="/">
         Home
       </a>
